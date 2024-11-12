c77b6f3a5b54b7afb5718744bf009838ea30555c
brillouinzone <david.stoker@sri.com>
2024-11-11 18:35:14 -0800

    new

---
+++ factorals_digits.py
@@ -0,0 +1,9 @@
+import math
+product_1_to_10 = math.prod(range(1, 11))
+product_11_to_20 = math.prod(range(11, 21))
+product_21_to_30 = math.prod(range(21, 31))
+product_31_to_40 = math.prod(range(31, 41))
+print(f"product_1_to_10={product_1_to_10}")
+print(f"product_11_to_20={product_11_to_20}")
+print(f"product_21_to_30={product_21_to_30}")
+print(f"product_31_to_40={product_31_to_40}")
